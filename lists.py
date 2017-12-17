439bfe193671f0cfa060be5772f0d84cd0916122
riaboshtan <sasha.riaboshtan@gmail.com>
2017-12-18 01:04:03 +0200

    priority

---
+++ lists.py
@@ -0,0 +1,20 @@
+import random
+
+class List:
+    d={}
+    for i in range(10):
+        d[i]=random.randint(0,100)
+    print(d)
+
+    def remove(self):
+        for i in range(10):
+            m = (max(self.d.keys(), key=lambda k: self.d[k]))
+            del self.d[m]
+        return 'Finish'
+
+l=List()
+print(l.remove())
+
+
+
+
